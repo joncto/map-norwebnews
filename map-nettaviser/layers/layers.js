3bfa116c22fa187a934cd4c3488565cd4fb255f9
Jon Carlstedt Tønnessen <60919868+joncto@users.noreply.github.com>
2023-02-27 17:05:48 +0100

    Rewrote link destinations to new host
    Links to NWA search results have been rewritten, from the old service to nettarkivet.beta.nb.no/web/

--- map-nettaviser/layers/layers.js
+++ map-nettaviser/layers/layers.js
@@ -12,26 +12,26 @@ var wms_layers = [];
                 url: 'http://tile.openstreetmap.org/{z}/{x}/{y}.png'
             })
         });
-var format_NorskeNettaviser_200521_1 = new ol.format.GeoJSON();
-var features_NorskeNettaviser_200521_1 = format_NorskeNettaviser_200521_1.readFeatures(json_NorskeNettaviser_200521_1, 
+var format_NorskeNettaviser_200521DVI_1 = new ol.format.GeoJSON();
+var features_NorskeNettaviser_200521DVI_1 = format_NorskeNettaviser_200521DVI_1.readFeatures(json_NorskeNettaviser_200521DVI_1, 
             {dataProjection: 'EPSG:4326', featureProjection: 'EPSG:3857'});
-var jsonSource_NorskeNettaviser_200521_1 = new ol.source.Vector({
+var jsonSource_NorskeNettaviser_200521DVI_1 = new ol.source.Vector({
     attributions: ' ',
 });
-jsonSource_NorskeNettaviser_200521_1.addFeatures(features_NorskeNettaviser_200521_1);
-var lyr_NorskeNettaviser_200521_1 = new ol.layer.Vector({
+jsonSource_NorskeNettaviser_200521DVI_1.addFeatures(features_NorskeNettaviser_200521DVI_1);
+var lyr_NorskeNettaviser_200521DVI_1 = new ol.layer.Vector({
                 declutter: true,
-                source:jsonSource_NorskeNettaviser_200521_1, 
-                style: style_NorskeNettaviser_200521_1,
+                source:jsonSource_NorskeNettaviser_200521DVI_1, 
+                style: style_NorskeNettaviser_200521DVI_1,
                 interactive: true,
-                title: '<img src="styles/legend/NorskeNettaviser_200521_1.png" /> NorskeNettaviser_2005-21'
+                title: '<img src="styles/legend/NorskeNettaviser_200521DVI_1.png" /> NorskeNettaviser_2005-21 DVI'
             });
 
-lyr_OSMStandard_0.setVisible(true);lyr_NorskeNettaviser_200521_1.setVisible(true);
-var layersList = [lyr_OSMStandard_0,lyr_NorskeNettaviser_200521_1];
-lyr_NorskeNettaviser_200521_1.set('fieldAliases', {'entityName': 'entityName', 'url': 'url', 'urlLoke': 'Åpne i Nettarkivet', 'utgivelsessted': 'utgivelsessted', 'fylke': 'fylke', 'geoLat(x)': 'geoLat(x)', 'geoLong(y)': 'geoLong(y)', 'nedslag': 'nedslag', 'Description': 'Description', 'year_start': 'year_start', 'date_end': 'date_end', 'url2': 'url2', 'url3': 'url3', 'url4': 'url4', 'nisje': 'nisje', 'field_16': 'field_16', });
-lyr_NorskeNettaviser_200521_1.set('fieldImages', {'entityName': 'TextEdit', 'url': 'Hidden', 'urlLoke': 'TextEdit', 'utgivelsessted': 'Hidden', 'fylke': 'Hidden', 'geoLat(x)': 'Hidden', 'geoLong(y)': 'Hidden', 'nedslag': 'Hidden', 'Description': 'Hidden', 'year_start': 'Hidden', 'date_end': 'Hidden', 'url2': 'Hidden', 'url3': 'Hidden', 'url4': 'Hidden', 'nisje': 'Hidden', 'field_16': 'Hidden', });
-lyr_NorskeNettaviser_200521_1.set('fieldLabels', {'entityName': 'no label', 'urlLoke': 'no label', });
-lyr_NorskeNettaviser_200521_1.on('precompose', function(evt) {
+lyr_OSMStandard_0.setVisible(true);lyr_NorskeNettaviser_200521DVI_1.setVisible(true);
+var layersList = [lyr_OSMStandard_0,lyr_NorskeNettaviser_200521DVI_1];
+lyr_NorskeNettaviser_200521DVI_1.set('fieldAliases', {'entityName': 'entityName', 'urlOriginal': 'Historisk URL', 'urlArkiv': 'Søk i Nettarkivet', 'utgivelsessted': 'utgivelsessted', 'fylke': 'fylke', 'geoLat(x)': 'geoLat(x)', 'geoLong(y)': 'geoLong(y)', 'nedslag': 'nedslag', 'Description': 'Description', 'year_start': 'year_start', 'date_end': 'date_end', 'url2': 'url2', 'url3': 'url3', 'url4': 'url4', 'nisje': 'nisje', 'field_16': 'field_16', });
+lyr_NorskeNettaviser_200521DVI_1.set('fieldImages', {'entityName': 'TextEdit', 'urlOriginal': 'Hidden', 'urlArkiv': 'TextEdit', 'utgivelsessted': 'Hidden', 'fylke': 'Hidden', 'geoLat(x)': 'Hidden', 'geoLong(y)': 'Hidden', 'nedslag': 'Hidden', 'Description': 'Hidden', 'year_start': 'Hidden', 'date_end': 'Hidden', 'url2': 'Hidden', 'url3': 'Hidden', 'url4': 'Hidden', 'nisje': 'Hidden', 'field_16': 'Hidden', });
+lyr_NorskeNettaviser_200521DVI_1.set('fieldLabels', {'entityName': 'no label', 'urlArkiv': 'no label', });
+lyr_NorskeNettaviser_200521DVI_1.on('precompose', function(evt) {
     evt.context.globalCompositeOperation = 'normal';
 });
--- map-nettaviser/styles/NorskeNettaviser_200521DVI_1_style.js
+++ map-nettaviser/styles/NorskeNettaviser_200521DVI_1_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_NorskeNettaviser_200521_1 = function(feature, resolution){
+var style_NorskeNettaviser_200521DVI_1 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
